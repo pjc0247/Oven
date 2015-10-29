7ed610739a32e9a96738d0b3ed49593510ff1bdb
pjc02478 <pjc0247@gmail.com>
2015-10-29 17:40:52 +0900

    refactoring

--- src/Oven.cs
+++ src/Oven.cs
@@ -37,6 +37,81 @@ namespace Oven
 
             return typeBuilder;
         }
+        private static MethodBuilder CreateMethod(
+            Type intf, Type impl,
+            TypeBuilder typeBuilder, MethodInfo method)
+        {
+            var paramTypes =
+                    method.GetParameters().Select(m => m.ParameterType).ToArray();
+            var methodBuilder = typeBuilder.DefineMethod(
+                method.Name,
+                MethodAttributes.Public |
+                MethodAttributes.Virtual |
+                MethodAttributes.NewSlot |
+                MethodAttributes.HideBySig |
+                MethodAttributes.Final,
+                method.ReturnType,
+                paramTypes);
+            var ilGen = methodBuilder.GetILGenerator();
+
+            /* args... -> object[] */
+            int argc = 0;
+            var args = ilGen.DeclareLocal(typeof(object[]));
+            var typeInfo = ilGen.DeclareLocal(typeof(Type));
+            var methodInfo = ilGen.DeclareLocal(typeof(MethodInfo));
+
+            ilGen.Emit(OpCodes.Ldc_I4, paramTypes.Length);
+            ilGen.Emit(OpCodes.Newarr, typeof(object));
+            ilGen.Emit(OpCodes.Stloc, args);
+
+            foreach (var param in method.GetParameters())
+            {
+                ilGen.Emit(OpCodes.Ldloc, args);
+                ilGen.Emit(OpCodes.Ldc_I4, argc);
+                ilGen.Emit(OpCodes.Ldarg, argc + 1);
+                if (paramTypes[argc].IsValueType)
+                    ilGen.Emit(OpCodes.Box, paramTypes[argc]);
+                ilGen.Emit(OpCodes.Stelem_Ref);
+
+                argc++;
+            }
+
+            /* ld_this */
+            ilGen.Emit(OpCodes.Ldarg_0);
+
+            var getTypeFromHandle =
+                typeof(Type).GetMethod("GetTypeFromHandle");
+            var getMethodFromHandle =
+                typeof(MethodBase).GetMethod(
+                    "GetMethodFromHandle",
+                    new[] { typeof(RuntimeMethodHandle) });
+            ilGen.Emit(OpCodes.Ldtoken, intf);
+            ilGen.Emit(OpCodes.Call, getTypeFromHandle);
+            ilGen.Emit(OpCodes.Castclass, typeof(Type));
+            ilGen.Emit(OpCodes.Ldtoken, method);
+            ilGen.Emit(OpCodes.Call, getMethodFromHandle);
+            ilGen.Emit(OpCodes.Castclass, typeof(MethodInfo));
+
+            ilGen.Emit(OpCodes.Ldloc, args);
+
+            /* performs proxy call */
+            ilGen.Emit(
+                OpCodes.Call,
+                impl.GetMethod(
+                    "OnMethod",
+                    BindingFlags.Instance | BindingFlags.Public));
+            /* return value of `RPCCall` will be automatically passed to caller,
+               but it needs to be unboxed to original type before returning. */
+            ilGen.Emit(OpCodes.Unbox, method.ReturnType);
+            ilGen.Emit(OpCodes.Ldobj, method.ReturnType);
+            ilGen.Emit(OpCodes.Ret);
+
+            typeBuilder.DefineMethodOverride(
+                methodBuilder,
+                intf.GetMethod(method.Name));
+
+            return methodBuilder;
+        }
 
         public static TBakeInterface Bake<TBakeInterface, TBakeImpl>()
             where TBakeImpl : IFilling
@@ -46,74 +121,9 @@ namespace Oven
             /* black magic */
             foreach (var method in typeof(TBakeInterface).GetMethods())
             {
-                var paramTypes =
-                    method.GetParameters().Select(m => m.ParameterType).ToArray();
-                var methodBuilder = typeBuilder.DefineMethod(
-                    method.Name,
-                    MethodAttributes.Public |
-                    MethodAttributes.Virtual |
-                    MethodAttributes.NewSlot |
-                    MethodAttributes.HideBySig |
-                    MethodAttributes.Final,
-                    method.ReturnType,
-                    paramTypes);
-                var ilGen = methodBuilder.GetILGenerator();
-
-                /* args... -> object[] */
-                int argc = 0;
-                var args = ilGen.DeclareLocal(typeof(object[]));
-                var typeInfo = ilGen.DeclareLocal(typeof(Type));
-                var methodInfo = ilGen.DeclareLocal(typeof(MethodInfo));
-
-                ilGen.Emit(OpCodes.Ldc_I4, paramTypes.Length);
-                ilGen.Emit(OpCodes.Newarr, typeof(object));
-                ilGen.Emit(OpCodes.Stloc, args);
-
-                foreach (var param in method.GetParameters())
-                {
-                    ilGen.Emit(OpCodes.Ldloc, args);
-                    ilGen.Emit(OpCodes.Ldc_I4, argc);
-                    ilGen.Emit(OpCodes.Ldarg, argc + 1);
-                    if (paramTypes[argc].IsValueType)
-                        ilGen.Emit(OpCodes.Box, paramTypes[argc]);
-                    ilGen.Emit(OpCodes.Stelem_Ref);
-
-                    argc++;
-                }
-
-                /* ld_this */
-                ilGen.Emit(OpCodes.Ldarg_0);
-
-                var getTypeFromHandle =
-                    typeof(Type).GetMethod("GetTypeFromHandle");
-                var getMethodFromHandle =
-                    typeof(MethodBase).GetMethod(
-                        "GetMethodFromHandle",
-                        new[] { typeof(RuntimeMethodHandle) });
-                ilGen.Emit(OpCodes.Ldtoken, typeof(TBakeInterface));
-                ilGen.Emit(OpCodes.Call, getTypeFromHandle);
-                ilGen.Emit(OpCodes.Castclass, typeof(Type));
-                ilGen.Emit(OpCodes.Ldtoken, method);
-                ilGen.Emit(OpCodes.Call, getMethodFromHandle);
-                ilGen.Emit(OpCodes.Castclass, typeof(MethodInfo));
-
-                ilGen.Emit(OpCodes.Ldloc, args);
-
-                /* performs proxy call */
-                ilGen.Emit(
-                    OpCodes.Call,
-                    typeof(TBakeImpl).GetMethod(
-                        "OnMethod",
-                        BindingFlags.Instance | BindingFlags.Public));
-                /* return value of `RPCCall` will be automatically passed to caller,
-                   but it needs to be unboxed to original type before returning. */
-                ilGen.Emit(OpCodes.Unbox, method.ReturnType);
-                ilGen.Emit(OpCodes.Ldobj, method.ReturnType);
-                ilGen.Emit(OpCodes.Ret);
-
-                typeBuilder.DefineMethodOverride(
-                    methodBuilder,
-                    typeof(TBakeInterface).GetMethod(method.Name));
+                CreateMethod(
+                    typeof(TBakeInterface), typeof(TBakeImpl),
+                    typeBuilder, method);
             }
 
             Type type = typeBuilder.CreateType();
